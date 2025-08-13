9432fa110d106896b4d067f4318cc3836fd71230
kALIPADA140 <aallu5593@gmail.com>
2025-08-13 09:02:15 +0530

    Add files via upload

---
+++ Subtract two numbers.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+   
+   int number,table,i=1;
+   
+   printf("Enter a number to print Table :- ");
+   scanf("%d",&number);
+main:
+  if(i<=10)
+  {
+   table=number*i;
+   printf("%d\n",table);
+   i++;
+  }
+return 0;
+
+}
